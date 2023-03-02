5cd86fc988f2acf0628f2b16bde0e425adc50216
Teja10 <tsaluru@ucdavis.edu>
2023-03-02 13:46:45 -0800

    Added surface normal intersection for example sphere

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,28 @@ use types::vec3::*;
 use types::ray::*;
 
 // check if a ray hits a sphere with specified center and radius
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     // compute the discriminant of the quadratic equation
     let oc: Vec3 = r.origin() - center;
     let a = r.direction().length_squared();
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.length_squared() - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    // if the discriminant is negative, the ray misses the sphere
+    // else there is an intersection and can evaluate the quadratic equation for t
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
+
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let N = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
     }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
